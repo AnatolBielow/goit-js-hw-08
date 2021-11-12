cb743af1688e3d0945cbb946a241e857076e82c0
Anatol Bielow <84202274+AnatolBielow@users.noreply.github.com>
2021-11-12 20:59:30 +0100

    zaraz sie powiesze

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-//import SimpleLightbox from 'simplelightbox';
+//npmimport SimpleLightbox from 'simplelightbox';
 // Дополнительный импорт стилей
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
